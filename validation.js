ce1e0ebc7de053087eafad9c989335d747e1da90
yusufnviiri <yusufnviiri@gmail.com>
2022-04-09 17:18:18 +0300

    Modify validation.JS file

--- validation.js
+++ validation.js
@@ -1,14 +1,13 @@
-const errorMessage=document.querySelector('#emailErr');
-const form=document.querySelector('form');
-const email=document.querySelector('#email');
+const errorMessage = document.querySelector('#emailErr');
+const form = document.querySelector('form');
+const email = document.querySelector('#email');
 
-form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    if(email.value.toLowerCase()==email.value){
-        form.submit();
-        
-    }else{        
-        errorMessage.innerHTML='email address must be lower case';
-        errorMessage.style.display='block';        
-    }
-})
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (email.value.toLowerCase() === email.value) {
+    form.submit();
+  } else {
+    errorMessage.innerHTML = 'email address must be lower case';
+    errorMessage.style.display = 'block';
+  }
+});
